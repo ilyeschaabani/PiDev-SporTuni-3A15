27200d7843e58aa856f77a66f9967f1e60d53055
ibtihelabd <161070783+ibtihelabd@users.noreply.github.com>
2024-03-06 19:24:37 +0100

    .

--- src/main/java/controllers/OneEventCardController.java
+++ src/main/java/controllers/OneEventCardController.java
@@ -86,117 +86,15 @@ public class OneEventCardController {
                 }
             }
 
-                  /* if (UserSession.getInstance().getEmail() == null) {
-
-                        try {
-                            user = userService.getOneUser("samir.temtem@esprit.tn");
-                        } catch (SQLException e) {
-                            e.printStackTrace();
-                        }
-                        System.out.println(user.getId());
-
-                    } else {
-                        try {
-                            user = userService.getOneUser(UserSession.getInstance().getEmail());
-                        } catch (SQLException e) {
-                            e.printStackTrace();
-                        }
-                        System.out.println(user.getId());
-
-                    }*/
+                 
 
 
         });
         //nb_left.setText(""+evenement.get);
 
-        // tester si un produit a un offre
+    
 
-        // add product to cart btn
-        /*AddToCart.setOnMouseClicked(event -> {
-            System.out.println("product added to command : " + produit.getId());
-            User user = new User();
-            UserService userService = new UserService();
-
-            if (UserSession.getInstance().getEmail() == null) {
-
-                try {
-                    user = userService.getOneUser("nabilkdp0@gmail.com");
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-                System.out.println(user.getId());
-
-            } else {
-                try {
-                    user = userService.getOneUser(UserSession.getInstance().getEmail());
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-                System.out.println(user.getId());
-
-            }
-            CommandsService commandsService = new CommandsService();
-            Commands command = new Commands();
-            command = commandsService.getOneCommand(user.getId());
-            System.out.println("Command is :" + command);
-            if (command == null) {
-
-                commandsService.addNewCommands(user.getId());
-            }
-
-            command = commandsService.getOneCommand(user.getId());
-
-            // System.out.println("Command not null :" + command);
-            // System.out.println("Command id :" + command.getId());
-            // System.out.println("produit id :" + produit.getId());
-            // System.out.println("user id :" + user.getId());
-
-            CommandsProduitService commandeProduitService = new CommandsProduitService();
-            // si le produit n'existe pas dans la commande on ajoute ce produit
-            if (commandeProduitService.getOneCommandProduit(produit.getId(), command.getId()) == null) {
-                commandeProduitService.addNewCommandsProduit(command.getId(), produit.getId());
-
-                Text addedCartModelText = (Text) ((Node) event.getSource()).getScene().lookup("#addedCartModelText");
-                addedCartModelText.setText("Product Added To Cart Successfully");
-
-                HBox addedCartModel = (HBox) ((Node) event.getSource()).getScene().lookup("#addedCartModel");
-                addedCartModel.setVisible(true);
-
-            } else { // produit existe deja dans la commande => msg affiché
-                Text addedCartModelText = (Text) ((Node) event.getSource()).getScene().lookup("#addedCartModelText");
-                addedCartModelText.setText("Product is ALready Added To Cart");
-
-                HBox addedCartModel = (HBox) ((Node) event.getSource()).getScene().lookup("#addedCartModel");
-                addedCartModel.setVisible(true);
-
-            }
-            // System.out.println(commandsService.getOneCommand(user.getId()));
-
-        });
-        // END add product to cart cart
-
-        // Open Product Details
-        open_productDetails.setOnMouseClicked(event -> {
-            System.out.println("ID du produit à afficher les details : " + produit.getId());
-            Collecte.setIdProduit(produit.getId());
-
-            FXMLLoader loader = new FXMLLoader(
-                    getClass().getResource("/gui/productInterfaces/UserProductDetails.fxml"));
-            try {
-                Parent root = loader.load();
-                // Accéder à la pane content_area depuis ce controller
-                Pane contentArea = (Pane) ((Node) event.getSource()).getScene().lookup("#content_area");
-
-                // Vider la pane et afficher le contenu de AddProduct.fxml
-                contentArea.getChildren().clear();
-                contentArea.getChildren().add(root);
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        });
-        // END - Open Product Details
-
-         */
+       
     }
 }
 
